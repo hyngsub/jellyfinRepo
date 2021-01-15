6bb8fe46d28c5699d06dc3d1ed9a8d40a340d7c7
1261954 <hengxie.che@oborcloud.com>
2021-01-15 11:42:20 +0800

    1

---
+++ src/main/java/com/chea/cinema/MyWebAppConfigurer.java
@@ -0,0 +1,17 @@
+package com.chea.cinema;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MyWebAppConfigurer 
+		extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+		super.addResourceHandlers(registry);
+	}
+
+}
